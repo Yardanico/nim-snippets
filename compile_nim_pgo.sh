c8b0bc4ad1cf8d5f834bb04220be7b4b00ddceb6
Danil Yarantsev <21169548+Yardanico@users.noreply.github.com>
2020-05-09 12:06:30 +0300

    Add script to compile Nim with PGO

---
+++ compile_nim_pgo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This is a simple script to compile the Nim compiler with
+# PGO (no LTO because as of now, if used with PGO, it breaks the compiler)
+# Use Arraymancer, npeg and compiler to provide data for PGO
+git clone --depth 1 https://github.com/mratsim/Arraymancer
+cd Arraymancer
+nimble install
+cd ..
+git clone --depth 1 https://github.com/zevv/npeg
+cd npeg
+nimble install
+cd ..
+# Compile the profiling version to generate PGO data
+nim c -d:danger --cc:clang --passC:"-fprofile-instr-generate" --passL:"-fprofile-instr-generate" \
+-o:bin/nimpgo1 compiler/nim.nim
+# Gather data from Arraymancer compilation
+./bin/nimpgo1 c -d:danger Arraymancer/tests/tests_cpu.nim
+mv default.profraw arraymancer.profraw
+# Gather data from npeg compilation
+./bin/nimpgo1 c -d:danger npeg/tests/tests.nim
+mv default.profraw npeg.profraw
+# Gather data from compiler compilation
+./bin/nimpgo1 c -d:danger -o:temp compiler/nim.nim
+mv default.profraw compiler.profraw
+# Merge all 3 profiling files into one file
+llvm-profdata merge arraymancer.profraw npeg.profraw compiler.profraw -output data.profdata
+# Compile the compiler with PGO
+nim c -d:danger --cc:clang --passC:"-fprofile-instr-use=data.profdata" \
+--passL:"-fprofile-instr-use=data.profdata" -o:bin/nim compiler/nim.nim
